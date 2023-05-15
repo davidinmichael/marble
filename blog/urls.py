3b766c9e45855ce76a0483a4cb919987f3bcb932
davidinmichael <davidinmichael@gmail.com>
2023-05-15 09:41:55 +0100

    I created a simple CRUD for the posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import *
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", PostListView.as_view(), name="home"),
     path("about/", views.about, name="about"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("create/", PostCreateView.as_view(), name="post_create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,34 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = "posts"
+    ordering = ['-date_posted']
 
-def home(request):
-    posts = Post.objects.all()
-    context = {
-        "title" : "Home",
-        "posts" : posts
-    }
-    return render(request, "blog/home.html", context)
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "blog/post-detail.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    context_object_name = "post"
+    template_name = "blog/post_form.html"
+    fields = ['title', 'content', 'post_image']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     context = {
         "title" : "About",
     }
     return render(request, "blog/about.html", context)
+
